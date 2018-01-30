b4186e5c9f3dcc42e38534e8bcb64ad90a010d1f
JayCampana <jaycampana4@gmail.com>
2018-01-30 23:39:49 +0100

    sistemato gradle + prova

--- app/src/main/java/com/scuola/simpleparking/MainActivity.java
+++ app/src/main/java/com/scuola/simpleparking/MainActivity.java
@@ -2,12 +2,17 @@ package com.scuola.simpleparking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mHello = findViewById(R.id.hello);
+        mHello.setText("Hello F. World");
     }
 }
