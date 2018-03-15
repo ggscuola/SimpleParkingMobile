41c34b32f390705cd502d8c74cba2f5ba5f1c1a1
MarcoS <markino.rotundo@tiscali.it>
2018-03-15 22:22:26 +0100

    ok

--- app/src/main/java/com/scuola/simpleparking/MainActivity.java
+++ app/src/main/java/com/scuola/simpleparking/MainActivity.java
@@ -1,10 +1,19 @@
 package com.scuola.simpleparking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText mInput;
+    private TextView mHello1;
+    private Button mButton;
 
 
     @Override
@@ -12,7 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView mHello = findViewById(R.id.hello);
-        mHello.setText("Hello Fucking World minimanimu");
+        getReferences();
+
+        mHello1.setText(getString(R.string.app_name));
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SecondaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void getReferences(){
+
+        mHello1 = findViewById(R.id.hello1);
+        mButton = findViewById(R.id.button);
+        mInput = findViewById(R.id.input);
+
     }
 }
